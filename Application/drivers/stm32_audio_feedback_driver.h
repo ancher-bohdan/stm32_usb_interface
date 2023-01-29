d84147eb3d137c4aa8df106bffd53cecec9402e6
Bohdan Ancher <ancher.bohdan@gmail.com>
2023-01-29 19:36:34 +0200

    [TASK] Add interface to FBCK driver to adjust bitrate manually

--- Application/drivers/stm32_audio_feedback_driver.h
+++ Application/drivers/stm32_audio_feedback_driver.h
@@ -4,9 +4,10 @@
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
 
-void FBCK_Init(void);
+void FBCK_Init(uint32_t ideal_bitrate);
 void FBCK_Start(void);
 void FBCK_Stop(void);
+void FBCK_adjust_bitrate(uint8_t free_buf_space);
 
 __weak void FBCK_send_feedback(uint32_t feedback);
 
